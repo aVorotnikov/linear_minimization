69a0bb1ad36b90c487d3499c8894177ab2706d5e
aVorotnikov <AndrewVorotnikov@yandex.ru>
2022-04-03 11:23:40 +0300

    Add task class

---
+++ task.py
@@ -0,0 +1,28 @@
+import math;
+
+class Task:
+    def __init__(self):
+        self.count = 0
+
+    @staticmethod
+    def f(x):
+        return x * math.sin(x) + 2 * math.cos(x)
+
+
+    def f_count(self, x):
+        self.count += 1
+        return x * math.sin(x) + 2 * math.cos(x)
+
+
+    def get_count(self):
+        return self.count
+
+
+    @staticmethod
+    def limits():
+        return (-5, -4)
+
+
+    @staticmethod
+    def accuracy():
+        return [0.1, 0.01, 0.001];
